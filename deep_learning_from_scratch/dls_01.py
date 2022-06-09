39da33d7168136cc076cbb53d3d8cfc4f04a23b7
Subhranil Sarkar <subhranil2605@gmail.com>
2022-06-09 18:50:09 +0530

    updated

--- deep_learning_from_scratch/dls_01.py
+++ deep_learning_from_scratch/dls_01.py
@@ -1,6 +1,16 @@
 from numpy import ndarray
 
 
+def assert_same_shape(array: ndarray, array_grad: ndarray):
+    assert array.shape == array_grad.shape, \
+        f'''
+        Two ndarrays should have the same shape;
+        instead, first array's shape is {tuple(array.shape)}
+        and second array's shape is {tuple(array_grad.shape)}
+    '''
+    return None
+
+
 class Operation(object):
     '''
     Base class for an 'operation' in a neural network
@@ -41,4 +51,38 @@ class Operation(object):
         '''
         The _input_grad method must be defined for each operation.
         '''
+        raise NotImplementedError()
+
+
+class ParamOperation(Operation):
+    '''
+    An Operation with parameters.
+    '''
+
+    def __int__(self, param: ndarray) -> ndarray:
+        """
+        The ParamOperation method
+        """
+        super().__init__()
+        self.param = param
+
+    def backward(self, output_grad: ndarray) -> ndarray:
+        """
+        Calls self._input_grad and self._param_grad.
+        Checks appropriate shapes.
+        """
+
+        assert_same_shape(self.output, output_grad)
+        self.input_grad = self._input_grad(output_grad)
+        self.param_grad = self._param_grad(output_grad)
+
+        assert_same_shape(self.input_, self.input_grad)
+        assert_same_shape(self.param, self.param_grad)
+
+        return self.input_grad
+
+    def _param_grad(self, output_grad: ndarray) -> ndarray:
+        """
+        Every subclass of ParamOperation must implement _param_grad.
+        """
         raise NotImplementedError()
