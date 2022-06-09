00d31988cecb254a463afbd8a16b16b6a6b7e460
Subhranil Sarkar <subhranil2605@gmail.com>
2022-06-09 17:27:40 +0530

    neural network

--- deep_learning_from_scratch/dls_01.py
+++ deep_learning_from_scratch/dls_01.py
@@ -1,7 +1,29 @@
+from numpy import ndarray
 
 
 class Operation(object):
     '''
     Base class for an "opeation" in a neural network
     '''
+
     def __init__(self):
+        pass
+
+    def forward(self, input_: ndarray):
+        '''
+        Stores input in the self.input_ instance variable
+        Calls the self._output() function
+        '''
+
+        self.input_ = input_
+        self.output = self._output()
+        return self.output
+
+    def backward(self, output_grad: ndarray) -> ndarray:
+        assert_same_shape(self.output, output_grad)
+
+
+
+    def _output(self) -> ndarray:
+        raise NotImplementedError()
+
